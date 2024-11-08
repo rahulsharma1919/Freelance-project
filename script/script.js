bdcff34b3f73fc326cd21911a09c16839b17ee02
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2024-11-08 17:06:31 +0530

    get in touch with pop up

--- script/script.js
+++ script/script.js
@@ -1,2 +1,7 @@
-// JavaScript interactions can be added here
-console.log("JavaScript is running");
+// Show the newsletter modal when the page finishes loading
+window.addEventListener("load", function () {
+  var newsletterModal = new bootstrap.Modal(
+    document.getElementById("newsletterModal")
+  );
+  newsletterModal.show();
+});

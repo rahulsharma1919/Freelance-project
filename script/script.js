3d1460394dad657b662a28647c0114914e48a1b8
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2024-11-09 14:03:12 +0530

    footer new

--- script/script.js
+++ script/script.js
@@ -5,3 +5,20 @@ window.addEventListener("load", function () {
   );
   newsletterModal.show();
 });
+
+// Toggle the arrow icon on collapse toggle
+const dropdownButtons = document.querySelectorAll(
+  '.btn-link[data-bs-toggle="collapse"]'
+);
+dropdownButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const icon = this.querySelector(".dropdown-icon");
+    if (this.getAttribute("aria-expanded") === "true") {
+      icon.classList.remove("fa-angle-down");
+      icon.classList.add("fa-angle-up");
+    } else {
+      icon.classList.remove("fa-angle-up");
+      icon.classList.add("fa-angle-down");
+    }
+  });
+});
